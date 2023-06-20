54b40802d9e97c9cc9eb1a7c92ac11ca02e0e721
krandon1986 <krandon1986@yahoo.co.uk>
2023-06-20 04:34:07 +0000

    Building the admin site

--- codestar/settings.py
+++ codestar/settings.py
@@ -30,6 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 DEBUG = True
 
 ALLOWED_HOSTS = ['codestar1986-cc1b06e98e4f.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['8000-krandon1986-djangoblog-gz8ydtb2uwa.ws-eu100.gitpod.io']
 
 
 # Application definition
@@ -43,6 +44,7 @@ INSTALLED_APPS = [
     'cloudinary_storage',
     'django.contrib.staticfiles',
     'cloudinary',
+    'django_summernote',
     'blog',
 ]
 
@@ -133,7 +135,7 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
-DEFAULT_FILE_STORAGE = 'cloudinary_storage.MediaCloudinaryStorage'
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
