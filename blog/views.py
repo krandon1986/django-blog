22b69a23ea445ddeb4b60a3c539447accabcdcd4
krandon1986 <krandon1986@yahoo.co.uk>
2023-06-26 16:32:13 +0000

    Adding messages

--- blog/views.py
+++ blog/views.py
@@ -65,9 +65,9 @@ class PostDetail(View):
             },
         )    
 
-class PostLike (View):
+class PostLike(View):
 
-    def post(self, request, slug):
+    def post(self, request, slug, *args, **kwargs):
         post = get_object_or_404(Post, slug=slug)
 
         if post.likes.filter(id=request.user.id).exists():
