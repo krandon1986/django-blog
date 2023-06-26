3b7e97cce0e3503d3db558b9cf6bcda3ca2c1dd3
krandon1986 <krandon1986@yahoo.co.uk>
2023-06-26 16:36:38 +0000

    deployment commit"

--- codestar/settings.py
+++ codestar/settings.py
@@ -30,7 +30,9 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 ACCOUNT_EMAIL_VERIFICATION = 'none'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
+
+X_FRAME_OPTIONS = 'SAMEORIGIN'
 
 ALLOWED_HOSTS = ['codestar1986-cc1b06e98e4f.herokuapp.com', 'localhost']
 ALLOWED_HOSTS = ['8000-krandon1986-djangoblog-gz8ydtb2uwa.ws-eu100.gitpod.io']
